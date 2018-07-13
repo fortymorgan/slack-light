82ee6b5b6cb26fb9e6bcda579bf639955621ee7e
Forty Morgan <dimazhmudikov@gmail.com>
2018-07-13 22:03:07 +0300

    Add switchig channels

--- src/components/ChannelsList.jsx
+++ src/components/ChannelsList.jsx
@@ -2,20 +2,31 @@ import React from 'react';
 import { ListGroup, ListGroupItem } from 'react-bootstrap';
 
 const Channel = (props) => {
-  const { active, name } = props;
+  const { active, name, handler } = props;
   return active ? <ListGroupItem active>{name}</ListGroupItem>
-    : <ListGroupItem>{name}</ListGroupItem>;
+    : <ListGroupItem onClick={handler}>{name}</ListGroupItem>;
 };
 
-const ChannelsList = (props) => {
-  const { channelsList, currentChannel } = props;
+export default class ChannelsList extends React.Component {
+  onSwitchChannel = id => () => {
+    const { setCurrentChannel } = this.props;
+    setCurrentChannel(id);
+  }
 
-  return (
-    <ListGroup bsClass="list-group col-3">
-      {channelsList.map(({ id, name }) =>
-        <Channel key={id} active={currentChannel === id} name={name} />)}
-    </ListGroup>
-  );
-};
+  render() {
+    const { channelsList, currentChannel } = this.props;
 
-export default ChannelsList;
+    return (
+      <ListGroup bsClass="list-group col-3">
+        {channelsList.map(({ id, name }) => (
+          <Channel
+            key={id}
+            active={currentChannel === id}
+            name={name}
+            handler={this.onSwitchChannel(id)}
+          />
+        ))}
+      </ListGroup>
+    );
+  }
+}
